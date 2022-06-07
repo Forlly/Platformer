706b13e7cdf0a42f05971b86f6d2f1cfcccab968
DESKTOP-ESQGB1I\remyp <forrrllly@gmail.com>
2022-06-08 00:14:57 +0300

    Refactoring Procedure Generation

--- Assets/Scripts/ProcedureGeneration.cs
+++ Assets/Scripts/ProcedureGeneration.cs
@@ -1,46 +1,26 @@
-using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using Random = System.Random;
 
 public class ProcedureGeneration : MonoBehaviour
 {
     [SerializeField] private int width;
     [SerializeField] private int height;
+
+    [Space] [Range(0f, 1f)]
+    [SerializeField] private float ChanceToChangeHeight;
+    
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private TileBase tilemapBase;
-    public int[,] map;
-    [SerializeField] public int[] highness;
-    [SerializeField] public int[] spread;
+    
+    private int[,] map;
+    
     void Start()
     { 
         map = new int[width, height];
-        map = GenerateArray(width, height, true);
-        RenderMap(map, tilemap, tilemapBase);
-        
-        map = GenerationMap2(map, 10, 2);
+
+        map = GenerationMap2(10, 2);
         
         UpdateMap(map, tilemap, tilemapBase);
-    }
-
-    public static int[,] GenerateArray(int width, int height, bool empty)
-    {
-        int[,] _map = new int[width, height];
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                if (empty)
-                {
-                    _map[x, y] = 0;
-                }
-                else
-                {
-                    _map[x, y] = 1;
-                }
-            }
-        }
-        return _map;
     }
     
     public static void UpdateMap(int[,] map, Tilemap tilemap, TileBase tile)
@@ -49,100 +29,53 @@ public class ProcedureGeneration : MonoBehaviour
         {
             for (int j = 0; j < map.GetUpperBound(1); j++)
             {
-
                 if (map[i, j] == 0)
-                {
-                    tilemap.SetTile(new Vector3Int(i, j, 0), null);
-                }
-                else
-                {
-                    tilemap.SetTile(new Vector3Int(i, j, 0), tile);
-                }
-            }
-        }
-    }
-    
-    
-    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile)
-    {
-        tilemap.ClearAllTiles();
-        for (int i = 0; i < map.GetUpperBound(0) ; i++)
-        {
-            for (int j = 0; j < map.GetUpperBound(1); j++) 
-            {
-                if (map[i, j] == 1) 
-                {
-                    tilemap.SetTile(new Vector3Int(i, j, 0), tile);
-                }
+                    continue;
+                
+                tilemap.SetTile(new Vector3Int(i, j, 0), tile);
             }
         }
     }
 
-    public int[,] GenerationMap2(int[,] map, int currentHeight, int minCount)
+    public int[,] GenerationMap2(int startHeight, int minCountHeightTiles)
     {
-        int previousHeight = currentHeight;
-        int count = 0;
-        for (int i = 0; i < map.GetUpperBound(0); i++)
+        int currentHeight = startHeight;
+        int countHeightTiles = 0;
+        
+        for (int j = 0; j < map.GetUpperBound(1); j++)
         {
+            while(j < startHeight)
+            {
+                map[0, j] = 1;
+                j++;
+            }
+        }
+        countHeightTiles++;
+        
+        for (int i = 1; i < map.GetUpperBound(0); i++)
+        {
+            if (countHeightTiles > minCountHeightTiles)
+            {
+                if (Random.Range(0f, 1f) <= ChanceToChangeHeight)
+                {
+                    int[] heghtI = {-2, -1, 1, 2}; 
+                    currentHeight += heghtI[Random.Range(0, heghtI.Length)];
+                    currentHeight = Mathf.Clamp(currentHeight ,1, height);
+                    countHeightTiles = 0;
+                }
+            }
+            
             for (int j = 0; j < map.GetUpperBound(1); j++)
             {
-                if (i == 0)
+                while (j < currentHeight)
                 {
-                    while(j < currentHeight && j < map.GetUpperBound(1))
-                    {
-                        map[i, j] = 1;
-                        j++;
-                    }
+                    map[i, j] = 1;
+                    j++;
                 }
-                else
-                {
-                    if (count > minCount)
-                    {
-                        previousHeight = new Random().Next(currentHeight + 1, previousHeight + 2);
-                        count = 0;
-                    }
-                    while(j < previousHeight && j < map.GetUpperBound(1))
-                    {
-                        map[i, j] = 1;
-                        j++;
-                    }
-                }
-                
             }
-            count++;
+            countHeightTiles++;
         }
 
-        return map;
-    }
-    public static int[,] GenerationMap(int[,] map, int[] height, int[] width)
-    {
-        int iW = 0;
-        for (int i = 0; i < map.GetUpperBound(0); i++)
-        {
-            if(iW < width.Length)
-            {
-                int tempCount = 0;
-                
-                while (tempCount < width[iW])
-                {
-                    map[i+tempCount, height[iW]-1] = 1;
-                    int j = 0;
-                    
-                    while (j < height[iW])
-                    {
-                        map[i+tempCount, j] = 1;
-                        j++;
-                    }
-                    
-                    tempCount++;
-                }
-
-                i += tempCount - 1;
-            }
-
-            iW++;
-            Debug.Log(iW);
-        }
         return map;
     }
 }
