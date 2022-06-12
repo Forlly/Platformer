2a149ba916e8dac32eb21a403b05537070090448
DESKTOP-ESQGB1I\remyp <forrrllly@gmail.com>
2022-06-12 03:02:57 +0300

    Fixing Cave Generation

--- Assets/Scripts/ProcedureGeneration.cs
+++ Assets/Scripts/ProcedureGeneration.cs
@@ -96,7 +96,7 @@ public class ProcedureGeneration : MonoBehaviour
         {
             if (Random.Range(0f, 1f) <= _chanceToGenerateCave)
             {
-                CaveGeneration(Random.Range(1, maxSizeOfCaves));
+                CaveGeneration2(Random.Range(1, maxSizeOfCaves));
             }
         }
         
@@ -106,20 +106,134 @@ public class ProcedureGeneration : MonoBehaviour
     private int[,] CaveGeneration2(int reqCaveAmount)
     {
         int size;
+        int direction;
         int caveX = Random.Range(1, map.GetUpperBound(0) - 1);
         int caveY = Random.Range(1, startHeight);
-        
-        while ( map[caveX, caveY + 1 ] != 0 || map[caveX, caveY] != 1)
+        int countOfSteps = 0;
+        int countOfStepsOneWay = 0;
+        float chanceToChangeDir;
+        bool stepLeft = true, stepRight = true;
+
+        if (map[caveX, caveY + 1 ] == 0 || map[caveX + 1, caveY] == 0 || map[caveX - 1, caveY] == 0 )
         {
-            caveY++;
+            map[caveX, caveY] = 0;
+        }
+        else
+        {
+            while ( map[caveX, caveY + 1 ] != 0 || map[caveX, caveY] != 1)
+            {
+                caveY++;
+            }
+
+            map[caveX, caveY] = 0;
         }
 
-        map[caveX, caveY] = 0;
-
         size = Random.Range(1, 3);
-        for (int i = 0; i < Mathf.Ceil(size/2); i++)
+        for (int i = 1; i < (size - 1); i++)
+        {
+            if (i % 2 == 0)
+            {
+                map[caveX + i, caveY] = 0;
+                caveX += i;
+            }
+            else
+            {
+                map[caveX - i, caveY] = 0;
+                caveX -= i;
+            }
+        }
+
+        while (countOfSteps < reqCaveAmount)
         {
             
+            size = Random.Range(2, 3);
+            direction = Random.Range(0,1);
+
+            if (direction == 0 && stepLeft)
+            {
+                map[caveX, caveY - 1] = 0;
+
+                if (caveX - size - 1 < 2)
+                {
+                    break;
+                }
+                
+                for (int i = 1; i < size; i++)
+                {
+                    map[caveX - i, caveY - 1] = 0;
+                }
+
+                caveX -= size - 1;
+                caveY -= 1;
+                if (caveY - 1 < 2)
+                {
+                    break;
+                }
+                
+                countOfStepsOneWay++;
+                
+                if (countOfStepsOneWay >= 2)
+                {
+                    chanceToChangeDir = Random.Range(0f, 1f);
+                    if (chanceToChangeDir<0.7f)
+                    {
+                        stepRight = true;
+                        stepLeft = false;
+                    
+                        countOfStepsOneWay = 0;
+                    }
+                }
+                else
+                {
+                    stepLeft = true;
+                    stepRight = false;
+                }
+
+                countOfSteps++;
+            }
+            else if(stepRight)
+            {
+                map[caveX, caveY - 1] = 0;
+
+                if (caveX + size - 1 > map.GetUpperBound(1) - 1)
+                {
+                    break;
+                }
+                
+                for (int i = 1; i < size; i++)
+                {
+                    map[caveX + i, caveY - 1] = 0;
+                }
+                
+                caveX += size - 1;
+                caveY -= 1;
+                if (caveY - 1 < 2)
+                {
+                    break;
+                }
+                
+                countOfStepsOneWay++;
+                
+                if (countOfStepsOneWay >= 2)
+                {
+                    chanceToChangeDir = Random.Range(0f, 1f);
+                    if (chanceToChangeDir<0.7f)
+                    {
+                        stepRight = false;
+                        stepLeft = true;
+
+                        countOfStepsOneWay = 0;
+                    }
+                }
+                else
+                {
+                    stepLeft = false;
+                    stepRight = true;
+                }
+                
+                countOfSteps++;
+            }
+            Debug.Log(countOfSteps);
         }
         return map;
     }
